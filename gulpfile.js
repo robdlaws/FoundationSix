2e88ec193f836206c812b632bbdb1ca96d087139
Robert Laws <robdlaws@gmail.com>
2016-11-17 16:22:14 +0300

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp        = require('gulp');
+var webserver   = require('gulp-webserver');
+
+gulp.task('default', function() {
+  gulp.src('')
+  .pipe(webserver({
+    livereload: true,
+    open: true,
+    directoryListing: false,
+    port: 8080
+  }));
+});
---
+++ typings/index.d.ts
@@ -0,0 +1,2 @@
+/// <reference path="globals/gulp/index.d.ts" />
+/// <reference path="modules/gulp/index.d.ts" />
